a004ee7ad188b1e86f29c9cd1d8c2cb88af61c3d
Rickard Andersson <dunklas@gmail.com>
2020-09-24 20:23:49 +0200

    Read complete pages into Vec<u8> instead of finding URLs to call..

--- gfa-poller/src/page_fetcher.rs
+++ gfa-poller/src/page_fetcher.rs
@@ -6,58 +6,64 @@ use select::{document, predicate};
 
 const BASE_URL: &str = "https://goteborg.se";
 
-pub struct UrlParserError {
+pub struct PageFetcherError {
     message: String,
 }
-impl fmt::Debug for UrlParserError {
+impl fmt::Debug for PageFetcherError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{{ file: {}, line: {} }}", file!(), line!())
     }
 }
 
-pub fn obtain_urls() -> Result<Vec<String>, UrlParserError> {
+pub fn obtain_pages() -> Result<Vec<Vec<u8>>, PageFetcherError> {
     let main_page = fetch_page(&format!("{}/wps/portal/start/avfall-och-atervinning/har-lamnar-hushall-avfall/farligtavfallbilen/farligt-avfall-bilen", BASE_URL))?;
     let paging_path = find_paging_path(main_page)?;
-    let mut urls: Vec<String> = Vec::new();
+    let mut pages: Vec<Vec<u8>> = Vec::new();
     for x in 0..20 {
         let paging_num: u16 = x * 30;
         let path = format_paging_path(paging_path.clone(), paging_num);
         let next_url = format!("{}{}", BASE_URL, path);
         let page = fetch_page(&next_url)?; 
-        if !has_items(page)? {
+        if !has_items(&page)? {
             break;
         }
-        urls.push(next_url);
+        pages.push(page);
     }
-    Ok(urls)
+    Ok(pages)
 }
 
-fn fetch_page(url: &String) -> Result<impl Read, UrlParserError> {
+fn fetch_page(url: &String) -> Result<Vec<u8>, PageFetcherError> {
     let response = get(&url).call();
     if !response.ok() {
-        return Err(UrlParserError{
+        return Err(PageFetcherError{
             message: format!("Response from {} not in 200-range", BASE_URL)
         });
     }
-    Ok(response.into_reader())
+    let mut page_bytes = Vec::<u8>::new();
+    match response.into_reader().read_to_end(&mut page_bytes) {
+        Ok(_size) => Ok(page_bytes),
+        Err(_e) => return Err(PageFetcherError{
+            message: format!("Could not read page")
+        })
+    }
 }
 
-fn find_paging_path(page: impl Read) -> Result<String, UrlParserError> {
-    let doc = match document::Document::from_read(page) {
+fn find_paging_path(page: Vec<u8>) -> Result<String, PageFetcherError> {
+    let doc = match document::Document::from_read(page.as_slice()) {
         Ok(doc) => doc,
-        Err(_e) => return Err(UrlParserError{
+        Err(_e) => return Err(PageFetcherError{
             message: format!("Could not parse page")
         })
     };
     let node = match doc.find(predicate::Class("c-pagination__link")).into_selection().first() {
         Some(node) => node,
-        None => return Err(UrlParserError{
+        None => return Err(PageFetcherError{
             message: format!("Could not find paging url")
         })
     };
     return match node.attr("href") {
         Some(url) => Ok(String::from(url)),
-        None => return Err(UrlParserError{
+        None => return Err(PageFetcherError{
             message: format!("Could not find href attribute of expected element")
         })
     };
@@ -69,10 +75,10 @@ fn format_paging_path(path: String, pagination: u16) -> String {
     return String::from(re.replace(&path, &new_pagination[..]));
 }
 
-fn has_items(page: impl Read) -> Result<bool, UrlParserError> {
-    let doc = match document::Document::from_read(page) {
+fn has_items(page: &Vec<u8>) -> Result<bool, PageFetcherError> {
+    let doc = match document::Document::from_read(page.as_slice()) {
         Ok(doc) => doc,
-        Err(_e) => return Err(UrlParserError{
+        Err(_e) => return Err(PageFetcherError{
             message: format!("Could not parse page")
         })
     };
@@ -82,11 +88,20 @@ fn has_items(page: impl Read) -> Result<bool, UrlParserError> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs::File;
+    use std::fs::{File, metadata};
+
+    fn read_file(path: &str) -> Vec<u8> {
+        let path = &format!("{}/src/resources/test/{}", env!("CARGO_MANIFEST_DIR"), path);
+        let mut file = File::open(path).unwrap();
+        let md = metadata(&path).unwrap(); 
+        let mut buffer = vec![0; md.len() as usize];
+        file.read(&mut buffer).unwrap();
+        buffer
+    }
 
     #[test]
     fn should_find_paging_path() {
-        let file = File::open(format!("{}/src/resources/test/body_with_items.html", env!("CARGO_MANIFEST_DIR"))).unwrap();
+        let file = read_file("body_with_items.html");
         let expected_path = String::from("/wps/portal/start/avfall-och-atervinning/har-lamnar-hushall-avfall/farligtavfallbilen/farligt-avfall-bilen/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8ziTYzcDQy9TAy9_f1MnAwcvXxd_JwM3Y3cPcz0w8EKDFCAo4FTkJGTsYGBu7-RfhTp-pFNIk4_HgVR-I0vyA0NDXVUVAQAXsfE3Q!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/p0/IZ7_42G01J41KON4B0AJMDNB1G2GP2=CZ6_42G01J41KON4B0AJMDNB1G2GH6=MDfilterDirection!filterOrganisationType!filterArea=Epagination!0==/");
         assert_eq!(expected_path, find_paging_path(file).unwrap());
     }
@@ -100,13 +115,13 @@ mod tests {
 
     #[test]
     fn should_detect_body_with_items() {
-        let file = File::open(format!("{}/src/resources/test/body_with_items.html", env!("CARGO_MANIFEST_DIR"))).unwrap();
-        assert_eq!(true, has_items(file).unwrap());
+        let file = read_file("body_with_items.html");
+        assert_eq!(true, has_items(&file).unwrap());
     }
 
     #[test]
     fn should_detect_body_without_items() {
-        let file = File::open(format!("{}/src/resources/test/body_without_items.html", env!("CARGO_MANIFEST_DIR"))).unwrap();
-        assert_eq!(false, has_items(file).unwrap());
+        let file = read_file("body_without_items.html");
+        assert_eq!(false, has_items(&file).unwrap());
     }
 }
