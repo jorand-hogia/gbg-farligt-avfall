8c63be83c18834f19eaae00567bbc913e2449096
Dunklas <dunklas@gmail.com>
2020-11-04 22:37:17 +0100

    Try set CORS with api gateway integration response (#77)

--- gfa-iac/lib/gfa-api-stack.ts
+++ gfa-iac/lib/gfa-api-stack.ts
@@ -1,7 +1,7 @@
 import { NestedStack, NestedStackProps } from '@aws-cdk/aws-cloudformation';
 import { IBucket } from '@aws-cdk/aws-s3';
 import { Construct } from '@aws-cdk/core';
-import { Cors, LambdaRestApi } from '@aws-cdk/aws-apigateway';
+import { Cors, LambdaIntegration, LambdaRestApi, PassthroughBehavior, RestApi } from '@aws-cdk/aws-apigateway';
 import { functionCreator } from './function-creator';
 
 interface ApiStackProps extends NestedStackProps {
@@ -28,17 +28,45 @@ export class ApiStack extends NestedStack {
         });
         props.stopsBucket.grantRead(getStops, props.stopsPath);
 
-        this.api = new LambdaRestApi(this, 'gfa-api', {
-            handler: getStops,
-            proxy: false,
+        this.api = new RestApi(this, 'gfa-api', {
             defaultCorsPreflightOptions: {
                 allowOrigins: Cors.ALL_ORIGINS,
-                allowMethods: [ 'GET' ],
-                allowHeaders: [ 'Content-Type', 'Accept' ],
+                allowMethods: ['GET'],
+                allowHeaders: ['Content-Type', 'Accept'],
             },
         });
-    
         const resource = this.api.root.addResource('stops');
-        resource.addMethod('GET');
+        resource.addMethod('GET', new LambdaIntegration(getStops, {
+            proxy: false,
+            allowTestInvoke: true,
+            passthroughBehavior: PassthroughBehavior.NEVER,
+            integrationResponses: [
+                {
+                    statusCode: '200',
+                    responseTemplates: {
+                        'application/json': JSON.stringify({ state: 'ok', stops: '$util.escapeJavaScript($input.body)' }),
+                    },
+                    responseParameters: {
+                        'method.response.header.Content-Type': "'application/json'",
+                        'method.response.header.Access-Control-Allow-Origin': "'*'",
+                        'method.response.header.Access-Control-Allow-Methods': "'GET'",
+                        'method.response.header.Access-Control-Allow-Headers': "'Content-Type, Accept'",
+                    },
+                },
+                {
+                    selectionPattern: '(\n|.)+',
+                    statusCode: "400",
+                    responseTemplates: {
+                        'application/json': JSON.stringify({ state: 'error', message: "$util.escapeJavaScript($input.path('$.errorMessage'))" })
+                    },
+                    responseParameters: {
+                        'method.response.header.Content-Type': "'application/json'",
+                        'method.response.header.Access-Control-Allow-Origin': "'*'",
+                        'method.response.header.Access-Control-Allow-Methods': "'GET'",
+                        'method.response.header.Access-Control-Allow-Headers': "'Content-Type, Accept'",
+                    },
+                },
+            ],
+        }));
     }
 }
