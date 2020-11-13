e566144cb573d19ec31dc43684998f6597a08ea5
Dunklas <dunklas@gmail.com>
2020-11-13 06:33:52 +0100

    Add base path mapping (#101)
    * Add base path mapping

--- gfa-iac/lib/gfa-api-stack.ts
+++ gfa-iac/lib/gfa-api-stack.ts
@@ -1,7 +1,7 @@
 import { NestedStack, NestedStackProps } from '@aws-cdk/aws-cloudformation';
 import { IBucket } from '@aws-cdk/aws-s3';
 import { Construct } from '@aws-cdk/core';
-import { Cors, LambdaIntegration, LambdaRestApi, RestApi } from '@aws-cdk/aws-apigateway';
+import { Cors, LambdaIntegration, LambdaRestApi, RestApi, SecurityPolicy, Stage } from '@aws-cdk/aws-apigateway';
 import { functionCreator } from './function-creator';
 import { Certificate } from '@aws-cdk/aws-certificatemanager';
 import { CertificateValidation } from '@aws-cdk/aws-certificatemanager';
@@ -59,12 +59,16 @@ export class ApiStack extends NestedStack {
             this.api.addDomainName('gfa-api-domain', {
                 domainName: apiDomainName,
                 certificate: apiCert, 
+                securityPolicy: SecurityPolicy.TLS_1_2,
             });
             new ARecord(this, 'gfa-api-domain-record', {
                 zone: hostedZone,
                 target: RecordTarget.fromAlias(new targets.ApiGateway(this.api)),
                 recordName: apiDomainName,
             });
+            this.api.domainName?.addBasePathMapping(this.api, {
+                basePath: 'stops'
+            })
         }
     }
 }
